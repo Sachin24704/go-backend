bfba963621df7db5f4679384e72b4d38eb9a1e9b
Sachin24704 <sachin24704@gmail.com>
2024-04-04 23:50:27 +0530

    TASK DB onnection completed

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/Sachin24704/go-backend
 
 go 1.22.1
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/howeyc/fsnotify v0.9.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
+	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
--- main.go
+++ main.go
@@ -11,45 +11,48 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var db  *sql.DB
+var db *sql.DB
 var err error
- 
+
 func addTweet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST"{
-		http.Error(w , "Method not allowed", http.StatusMethodNotAllowed)
-		return 
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
-    userIDStr := r.PathValue("id")
+	userIDStr := r.PathValue("id")
 	if userIDStr == "" {
 		http.Error(w, "User ID not found in URL", http.StatusBadRequest)
 		return
 	}
-    userID,err:=strconv.Atoi(userIDStr)
-    if  err!= nil {
-	 	http.Error(w , "cannot seperate index from URL", http.StatusInternalServerError)
-	 	return
-    }
-   // check if user exist in db
-   var exists bool
-   row:=db.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE user_id = $1)", userID)
-   err=row.Scan(&exists)
-   if err != nil {
-	   http.Error(w, err.Error(), http.StatusInternalServerError)
-	   return
-   }
-   if !exists {
-	   http.Error(w, "User does not exist", http.StatusNotFound)
-	   return
-   }
-    body,err:=io.ReadAll(r.Body)
-	if err!=nil {
-		http.Error(w, "error while reading request body ", http.StatusInternalServerError)
-		return  
+	userID, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		http.Error(w, "cannot parse ID", http.StatusInternalServerError)
+		return
 	}
-	body_map:=make(map[string]string)
-	json.Unmarshal(body,&body_map)
-    tweet, ok := body_map["tweet"]
-	if !ok || len(tweet) ==0 {
+	var exists bool
+	row := db.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE user_id = $1)", userID)
+	err = row.Scan(&exists)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if !exists {
+		http.Error(w, "User does not exist", http.StatusNotFound)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "error while reading request body ", http.StatusInternalServerError)
+		return
+	}
+	body_map := make(map[string]string)
+	err = json.Unmarshal(body, &body_map)
+	if err != nil {
+		http.Error(w, "error while unmarshalling data from json ", http.StatusInternalServerError)
+		return
+	}
+	tweet, ok := body_map["tweet"]
+	if !ok || len(tweet) == 0 {
 		http.Error(w, "Tweet not found in request body", http.StatusBadRequest)
 		return
 	}
@@ -58,83 +61,79 @@ func addTweet(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to add tweet to database", http.StatusInternalServerError)
 		return
 	}
-	io.WriteString(w,"Successfully added the Tweet")
+	io.WriteString(w, "Successfully added the Tweet")
 }
 
 func getTweet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET"{
-		http.Error(w , "Method not allowed", http.StatusMethodNotAllowed)
-		return 
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 	userIDStr := r.PathValue("id")
 	if userIDStr == "" {
 		http.Error(w, "User ID not found in URL", http.StatusBadRequest)
 		return
 	}
-    userID,err:=strconv.Atoi(userIDStr)
-    if  err!= nil {
-	 	http.Error(w , "cannot seperate index from URL", http.StatusInternalServerError)
-	 	return
-    }
-// check if user exist in db
-    var exists bool
-    row:=db.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE user_id = $1)", userID)
-	err=row.Scan(&exists)
-    if err != nil {
-	    http.Error(w, err.Error(), http.StatusInternalServerError)
-	    return
-    }
-    if !exists {
-	    http.Error(w, "User does not exist", http.StatusNotFound)
-	    return
-    }
-	rows,err:=db.Query("SELECT tweet FROM tweets WHERE user_id = $1",userID)
-	if err !=nil {
+	userID, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		http.Error(w, "cannot seperate index from URL", http.StatusInternalServerError)
+		return
+	}
+	var exists bool
+	row := db.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE user_id = $1)", userID)
+	err = row.Scan(&exists)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+		return
+	}
+	if !exists {
+		http.Error(w, "User does not exist", http.StatusNotFound)
+		return
+	}
+	rows, err := db.Query("SELECT tweet FROM tweets WHERE user_id = $1", userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
-	tweets := make([]string,0)
+	tweets := make([]string, 0)
 	for rows.Next() {
 		var tweet string
-		err:=rows.Scan( &tweet )
-		if err!=nil {
-			http.Error(w , "error while scanning tweets", http.StatusInternalServerError)
-	 	    return
+		err := rows.Scan(&tweet)
+		if err != nil {
+			http.Error(w, "error while scanning tweets", http.StatusInternalServerError)
+			return
 		}
-		tweets=append(tweets,tweet)
+		tweets = append(tweets, tweet)
 	}
-	if len(tweets) ==0 {
-		io.WriteString(w,"No tweets for this user")
+	if len(tweets) == 0 {
+		io.WriteString(w, "No tweets for this user")
 		return
 	}
 	//convert to json
-	jsonData,err := json.Marshal(tweets)
-	if err!=nil {
-		http.Error(w , "cannot convert tweets to json", http.StatusInternalServerError)
+	jsonData, err := json.Marshal(tweets)
+	if err != nil {
+		http.Error(w, "cannot convert tweets to json", http.StatusInternalServerError)
 		return
-    }
-	io.WriteString(w,string(jsonData))
+	}
+	io.WriteString(w, string(jsonData))
 }
 
-func main(){
+func main() {
 	// connect to db
 	db, err = sql.Open("postgres", "postgres://postgres:123456@localhost:5432/twitter?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
-    defer db.Close()
-	// status will check if db is connected
-	status:=true
-	err =db.Ping()
-	if err != nil{
-		log.Println(err)
-		status=false
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Println("DB connected status:",status)
-   // in body json of key "tweet"- eg{"tweet":"hello world"} 
-    http.HandleFunc("/addTweet/{id}",addTweet)
-   //in case id is passed as endpoint
-    http.HandleFunc("/getTweet/{id}",getTweet)
+	log.Println("DB connected")
+	// in body json of key "tweet"- eg{"tweet":"hello world"}
+	http.HandleFunc("/addTweet/{id}", addTweet)
+	//in case id is passed as endpoint
+	http.HandleFunc("/getTweet/{id}", getTweet)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
