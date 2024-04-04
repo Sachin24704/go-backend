55ea10687cb31da490b6aec44f15280edd0a04aa
Sachin24704 <sachin24704@gmail.com>
2024-04-04 17:47:21 +0530

    Task 1- backend server created

--- main.go
+++ main.go
@@ -28,115 +28,77 @@ func addTweet(w http.ResponseWriter, r *http.Request) {
 		http.Error(w , "Method not allowed", http.StatusMethodNotAllowed)
 		return 
 	}
-
-	userIDStr := r.PathValue("id")
+    userIDStr := r.PathValue("id")
 	if userIDStr == "" {
 		http.Error(w, "User ID not found in URL", http.StatusBadRequest)
 		return
 	}
-
-	userID,err:=strconv.Atoi(userIDStr)
+    userID,err:=strconv.Atoi(userIDStr)
     if  err!= nil {
-	 	http.Error(w , "cannot seperate index from URL", 400)
+	 	http.Error(w , "cannot seperate index from URL", http.StatusInternalServerError)
 	 	return
     }
-
-	body,err:=io.ReadAll(r.Body)
+    body,err:=io.ReadAll(r.Body)
 	if err!=nil {
-		http.Error(w, "error while reading request body ", 400)
+		http.Error(w, "error while reading request body ", http.StatusInternalServerError)
 		return  
 	}
 	body_map:=make(map[string]string)
 	json.Unmarshal(body,&body_map)
-
-	tweet:=body_map["tweet"]
+    tweet:=body_map["tweet"]
 	if len(tweet) == 0{
-		http.Error(w,"tweet invalid",400)
+		http.Error(w,"tweet invalid",http.StatusBadRequest)
 		return
 	}
-
-	for i,u:= range user{
+    for i,u:= range user{
 		if u.Id == userID{
 			user[i].Tweets=append(user[i].Tweets, tweet)
 			jsonData,err :=json.Marshal(user[i].Tweets)
 			if err!=nil {
-				http.Error(w , "cannot convert tweets to json", 400)
+				http.Error(w , "cannot convert tweets to json", http.StatusInternalServerError)
 		        return
-
-			}
+            }
 			io.WriteString(w, string(jsonData))
 			return
 		}
 	}
-	http.Error(w,"User not found",400)
-	
+	http.Error(w,"User not found",http.StatusNotFound)
 }
+
 func getTweet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET"{
 		http.Error(w , "Method not allowed", http.StatusMethodNotAllowed)
 		return 
 	}
-	
-
 	userIDStr := r.PathValue("id")
 	if userIDStr == "" {
 		http.Error(w, "User ID not found in URL", http.StatusBadRequest)
 		return
 	}
-
-	userID,err:=strconv.Atoi(userIDStr)
+    userID,err:=strconv.Atoi(userIDStr)
     if  err!= nil {
-	 	http.Error(w , "cannot seperate index from URL", 400)
+	 	http.Error(w , "cannot seperate index from URL", http.StatusInternalServerError)
 	 	return
     }
-
-	// when id is passed in body using "id"
-
-	/*body, err := io.ReadAll(r.Body)
-	if err!=nil {
-		http.Error(w , "error in reading r.body", http.StatusMethodNotAllowed)
-		return
-
-	}
-	body_map:=make(map[string]string)
-
-	json.Unmarshal(body, &body_map)
-    
-	index, err := strconv.Atoi(body_map["id"])
-	if err!=nil {
-		http.Error(w , "could not find id from body", http.StatusMethodNotAllowed)
-		return
-
-	} */
-	
 	for _,u := range user {
 		if u.Id ==userID{
 			jsonData, err :=json.Marshal(u.Tweets)
 			if err!=nil {
-				http.Error(w , "cannot convert tweets to json", 400)
+				http.Error(w , "cannot convert tweets to json", http.StatusInternalServerError)
 		        return
-
 			}
 			io.WriteString(w, string(jsonData))
 			return
-
 		}
-
 	}
-	http.Error(w,"User not found",400)
+	http.Error(w,"User not found",http.StatusNotFound)
 }
 
 
 func main(){
-
   // in body json of key "tweet"- eg{"tweet":"hello world"} 
   http.HandleFunc("/addTweet/{id}",addTweet)
-
-  // in body json of key "id"- eg {"id":"1" }
-  //http.HandleFunc("/getTweet",getTweet)
-
-  //in case id is passed as endpoint
+ //in case id is passed as endpoint
   http.HandleFunc("/getTweet/{id}",getTweet)
-  
   log.Fatal(http.ListenAndServe(":8000", nil))
 }
